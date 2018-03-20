c4c300cd8759e3241ac103f6eff529c5828135db
1103-14 <1103-14@1103-14-PC>
2018-03-20 16:42:58 +0900

    index tile

--- spring_Project/src/total/controller/IndexController.java
+++ spring_Project/src/total/controller/IndexController.java
@@ -1,6 +1,8 @@
 package total.controller;
 
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
 
 	@RequestMapping({"/index","/"})
-	public String indexHandle() {
-		
-		return "index";
+	public String indexHandle(Map map) {
+		map.put("body", "index.jsp");
+		return "t_el";
 	}
 }
