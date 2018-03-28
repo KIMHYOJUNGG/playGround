0b2a402ecd3e1744f74453d351bf3c5e99e79fad
1103-8 <1103-8@1103-8-PC>
2018-03-28 16:13:35 +0900

    게시글 제거 기능, ckeditor

--- spring_Project/WebContent/resources/ckeditor/config.js
+++ spring_Project/WebContent/resources/ckeditor/config.js
@@ -3,19 +3,53 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	config.allowedContent = true;
-	config.enterMode = CKEDITOR.ENTER_BR;
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
-	config.toolbar =[
-        ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','Undo','Redo','SelectAll','RemoveFormat'],
-        '/',
-        ['Bold','Italic','Underline','Strike', 'Subscript','Superscript'],
-        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['NumberedList','BulletedList','Outdent','Indent','Blockquote','CreateDiv'],
-        ['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
-        ['Styles','Format','Font','FontSize','TextColor','BGColor','Maximize', 'ShowBlocks']
-    ];
-};
+/*CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	// The toolbar groups arrangement, optimized for two toolbar rows.
+	config.toolbarGroups = [
+		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+		{ name: 'links' },
+		{ name: 'insert' },
+		{ name: 'forms' },
+		{ name: 'tools' },
+		{ name: 'document',	   groups: [ 'mode', 'documsent', 'doctools' ] },
+		{ name: 'others' },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'styles' },
+		{ name: 'colors' },
+		{ name: 'about' }
+	];
+
+	// Remove some buttons provided by the standard plugins, which are
+	// not needed in the Standard(s) toolbar.
+	config.removeButtons = 'Underline,Subscript,Superscript';
+
+	// Set the most common block elements.
+	config.format_tags = 'p;h1;h2;h3;pre';
+
+	// Simplify the dialog windows.
+	config.removeDialogTabs = 'image:advanced;link:advanced';
+};*/
+
+	 
+
+	CKEDITOR.editorConfig = function( config ) {
+	 // Define changes to default configuration here. For example:
+	    config.language = 'ko';
+	    config.uiColor = '#FCE4C5';
+	    config.font_names = '맑은 고딕; 돋움; 바탕; 돋음; 궁서; Nanum Gothic Coding; Quattrocento Sans;' + CKEDITOR.config.font_names; 
+	    config.height = '400px';  //Editor 높이  
+	    config.width = '100%';
+	    config.enterMode =CKEDITOR.ENTER_BR;
+	    config.toolbarCanCollapse = true;
+	    config.docType = "<!DOCTYPE html>";
+	    config.filebrowserFlashUploadUrl = 'imageUpload'
+	    config.filebrowserImageUploadUrl = 'imageUpload' //여기 경로로 파일을 전달하여 업로드 시킨다.
+	    config.removePlugins = 'save'
+	};
